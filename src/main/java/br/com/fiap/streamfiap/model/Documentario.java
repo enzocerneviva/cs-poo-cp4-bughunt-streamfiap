4a9f1dc8cecb98f5100a709b55eaaf474ec2a73d
Enzo Cerneviva <contcerneviva@gmail.com>
2026-09-08 16:43:47 -0300

    fix: bug05 - Documentario nao sobrescrevia calcularPrecoAluguel e cobrava 9.90 em vez de ser gratis

--- src/main/java/br/com/fiap/streamfiap/model/Documentario.java
+++ src/main/java/br/com/fiap/streamfiap/model/Documentario.java
@@ -15,6 +15,12 @@ public class Documentario extends Conteudo {
         this.tema = tema;
     }
 
+    // documentário é gratuito
+    @Override
+    public double calcularPrecoAluguel() {
+        return 0.0;
+    }
+
     public String getTema() { return tema; }
     public void setTema(String tema) { this.tema = tema; }
 }
